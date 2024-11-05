2cef0175768cd10475db8719aad9d476a4ab1eac
Daniel <daniel@devanddeliver>
2024-11-05 13:29:23 +0100

    feat: add styled internal button

--- vite-host/src/App.jsx
+++ vite-host/src/App.jsx
@@ -3,12 +3,24 @@ import RD from 'react-dom/client';
 
 import { ButtonFromRemote } from '@namespace/viteViteRemote/ButtonFromRemote';
 import { ExampleButton } from 'example-styled-components-package';
+import styled, { css } from 'styled-components';
 
 console.log('Share React', R, RD);
+
+export const InternalButtonElement = styled.button`
+  ${() => css`
+    background: red;
+    border: 0;
+    margin: 0;
+    padding: 20px;
+  `}
+`;
+
 
 export default function () {
   return (
     <div>
+      <ExampleButtonElement>Internal Button Element</ExampleButtonElement>
       <ExampleButton />
       <Suspense fallback={<div>Loading...</div>}>
         <ButtonFromRemote />
